3c90c0d37595f282b91c14066b998787289e138a
sungjh <jinhyuk9714@gmail.com>
2025-08-01 11:59:32 +0900

    workflow test commit

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BackendportfolioApplication {
     public static void main(String[] args) {
-        // workflow test3
+        // workflow test4
         System.out.println("성진혁");
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
